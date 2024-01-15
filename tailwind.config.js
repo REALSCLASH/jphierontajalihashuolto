cdc793f1314ad694546edefe21d2ceb6bbbb606c
REALSCLASH <samuelpellikka@gmail.com>
2024-01-15 17:40:44 +0200

    Final

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+
+  theme: {
+    extend: {
+      colors: {
+        body: '',
+        theme: '#DECBB7',
+        nc: '#2f2a27',
+        harmaa: '#333333',
+       
+      },
+      
+      
+    },
+  },
+  plugins: [],
+}
+
